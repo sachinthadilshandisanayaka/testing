91e979c9fd28bdc9e13566ea466982dbba46bf07
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-06 23:36:05 +0530

    add hot reload method and StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Face Scanner'),
-          backgroundColor: Color.fromRGBO(0, 206, 201, 1.0),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text(
-            'Scanning your face',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Color.fromRGBO(99, 110, 114, 1.0),
-              fontFamily: 'SansitaSwashed',
-            ),
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // hot reload, we no need to reload the code, auto reload it
+    // because of buil(BuildContext)
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Face Scanner',
+          style: TextStyle(
+            fontFamily: 'Nunito',
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.0,
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('click'),
-          backgroundColor: Color.fromRGBO(27, 156, 252, 1.0),
-        ),
+        backgroundColor: Color.fromRGBO(0, 206, 201, 1.0),
+        centerTitle: true,
       ),
-    ));
+      body: Center(
+        child: Image.asset('assets/photo-03.jpeg'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Color.fromRGBO(27, 156, 252, 1.0),
+      ),
+    );
+  }
+}
