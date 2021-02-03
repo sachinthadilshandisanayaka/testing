d2d56d10bcfeb9a38bd05a9d64004b60039ba0e1
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2021-02-03 20:59:51 +0530

    updated

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCgLCjbYfkzWUan8dIe2RTpRSx1Wne14Wo")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
